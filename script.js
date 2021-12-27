148e5a4a82767d5e8f33ffb07e89410724c14c30
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-27 17:38:36 -0500

    create array to store quiz questions

---
+++ script.js
@@ -0,0 +1,45 @@
+var question1 = {
+    question: 'The condition in an if/else statement is enclosed with ______.',
+    answer0: '1. quotes',
+    answer1: '2. curly brackets',
+    answer2: '3. parenthesis',
+    answer3: '4. square brackets',
+    correct: 2,
+  };
+  
+  var question2 = {
+    question: 'Arrays in JavaScript can be used to store ______.',
+    answer0: '1. numbers and strings',
+    answer1: '2. other arrays',
+    answer2: '3. booleans',
+    answer3: '4. all of the above',
+    correct: 3, 
+  };
+  
+  var question3 = {
+    question: 'String values must be enclosed within ______ when being assigned to variables.',
+    answer0: '1. commas',
+    answer1: '2. curly brackets',
+    answer2: '3. quotes',
+    answer3: '4. parenthesis',
+    correct: 2, 
+  };
+  
+  var question4 = {
+    question: 'Commonly used data types DO Not include',
+    answer0: '1. strings',
+    answer1: '2. booleans',
+    answer2: '3. alerts',
+    answer3: '4. numbers',
+    correct: 2,
+  };
+  
+  var question5 = {
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answer0: '1. JavaScript',
+    answer1: '2. terminal/bash',
+    answer2: '3. for loops',
+    answer3: '4. console.log',
+    correct: 3,
+  };
+  
