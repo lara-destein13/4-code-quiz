02ccfecfe0885de46dba3544e55007f67ca6bdd6
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-27 19:57:52 -0500

    correct indenting in js file

--- script.js
+++ script.js
@@ -5,41 +5,71 @@ var question1 = {
     answer2: '3. parenthesis',
     answer3: '4. square brackets',
     correct: 2,
-  };
-  
-  var question2 = {
+};
+
+var question2 = {
     question: 'Arrays in JavaScript can be used to store ______.',
     answer0: '1. numbers and strings',
     answer1: '2. other arrays',
     answer2: '3. booleans',
     answer3: '4. all of the above',
     correct: 3, 
-  };
-  
-  var question3 = {
+};
+
+var question3 = {
     question: 'String values must be enclosed within ______ when being assigned to variables.',
     answer0: '1. commas',
     answer1: '2. curly brackets',
     answer2: '3. quotes',
     answer3: '4. parenthesis',
     correct: 2, 
-  };
-  
-  var question4 = {
+};
+
+var question4 = {
     question: 'Commonly used data types DO Not include',
     answer0: '1. strings',
     answer1: '2. booleans',
     answer2: '3. alerts',
     answer3: '4. numbers',
     correct: 2,
-  };
-  
-  var question5 = {
+};
+
+var question5 = {
     question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
     answer0: '1. JavaScript',
     answer1: '2. terminal/bash',
     answer2: '3. for loops',
     answer3: '4. console.log',
     correct: 3,
-  };
-  
+};
+
+var allQuestions = [
+    question1,
+    question2,
+    question3,
+    question4,
+    question5
+];
+
+var interval;
+var questionNumber = 0;
+var time = 0;
+var maxTime = 60;
+var score;
+var initials = '';
+
+function setDisplay(id, value) {
+    var element = document.getElementById(id);
+}
+
+
+
+
+
+
+
+
+//  buttonEl.addEventListener("click", function() {
+//      alert("button clicked");
+//  }
+
